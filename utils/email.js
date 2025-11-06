36a48ba3648e2a46cd24e66ee9bbf474fa836726
mohammad kabiri <moh83amad@gmail.com>
2025-11-06 13:14:49 +0330

    replacing with maileroo

--- utils/email.js
+++ utils/email.js
@@ -29,35 +29,45 @@ class Email {
                 requireTLS: true,
             });
         }
-        else if (process.env.NODE_ENV === "production") {
-            return nodeMailer.createTransport({
-                host: "smtp.gmail.com",
-                port: 587,
-                secure: true, // SSL
-                auth: {
-                    user: process.env.EMAIL_FROM,
-                    pass: process.env.APP_PASSWORD, // app password
-                },
-                tls: {
-                    rejectUnauthorized: false,
-                },
-            });
-            // return new MailerSend({ apiKey: process.env.MAILERSEND_API_KEY })
-
-        }
+        // else if (process.env.NODE_ENV === "production") {
+        //     return nodeMailer.createTransport({
+        //         host: "smtp.gmail.com",
+        //         port: 587,
+        //         secure: true, // SSL
+        //         auth: {
+        //             user: process.env.EMAIL_FROM,
+        //             pass: process.env.APP_PASSWORD, // app password
+        //         },
+        //         tls: {
+        //             rejectUnauthorized: false,
+        //         },
+        //     });
+        //
+        //
+        // }
 
     }
+
+    async send_production (html, subject) {
+        const {MailerooClient, EmailAddress, Attachment} = await import('maileroo-sdk');
+
+        const client = new MailerooClient(process.env.MAILEROO_API_KEY);
+
+        const referenceId = await client.sendBasicEmail({
+            from: new EmailAddress(process.env.MAILEROO_SENDER_EMAIL, process.env.APP_NAME),
+            to: [new EmailAddress(this.to, this.firstName)],
+            subject: subject,
+            html: html,
+            plain: htmlToText.htmlToText(html)
+        });
+
+
+        };
+
 
     async send(template, subject){
         const transporter = this.newTransport();
 
-        transporter.verify((error, success) => {
-            if (error) {
-                console.error("SMTP connection error:", error);
-            } else {
-                console.log("SMTP server is ready to send messages");
-            }
-        });
         const html = await ejs.renderFile(`${__dirname}/../views/email/${template}.ejs`, {
             name: this.firstName,
             url: this.url,
@@ -65,15 +75,18 @@ class Email {
             subject
         });
 
-            const mailOptions = {
-                from: this.from,
-                to: this.to,
-                subject,
-                html,
-                text: htmlToText.htmlToText(html)
-            };
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.htmlToText(html)
+        };
 
-            return await this.newTransport().sendMail(mailOptions);
+        if (process.env.NODE_ENV === "production") {
+            return await this.send_production(html, subject);
+        }
+        return await this.newTransport().sendMail(mailOptions);
 
     }
 
